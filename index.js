8b765923cf02aec8c2ac012acf82176f90e4e87c
sandeeep-prajapti <6392424180sandeep@gmail.com>
2023-12-12 10:51:56 +0530

    hjghsfdhd

--- index.js
+++ index.js
@@ -7,10 +7,69 @@
 // else{
 //     console.log(`number is odd : ${num}`);
 // }
-let open = true;
-if(open){
-console.log(`i will attend the class`);
+// let open = true;
+// if(open){
+// console.log(`i will attend the class`);
+// }
+// else{
+// console.log(`collage is closed today`);
+// }
+
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+// if(5=="5"){
+//     console.log ("5 is equal to 5");
+// }
+// else{
+//     console.log("5 is not equal to 5")
+// }
+
+// if(5==="5"){
+//     console.log ("5 is equal to 5");
+// }
+// else{
+//     console.log("5 is not equal to 5")
+// }
+// if(4=="4"){
+//     console.log ("4 is equal to 4");
+// }
+// else{
+//     console.log("4 is not equal to 4")
+// }
+
+// if(4==="4"){
+//     console.log ("4 is equal to 4");
+// }
+// else{
+//     console.log("4 is not equal to 4")
+// }
+
+// let clgopen = 4;
+// console.log(clgopen)
+
+// let if = 4
+// console.log(const);
+
+// let if = 4
+// console.log(if);
+
+
+
+let year = prompt("enter your current year");
+var result = year==1?"you have 3 year reaming":1;
+if(result !=1){
+    console.log(result);
 }
-else{
-console.log(`collage is closed today`);
+var result = year==2?"you have 2 year reaming":1;
+if(result !=1){
+    console.log(result);
+}
+var result = year==3?"you have 1 year reaming":1;
+if(result !=1){
+    console.log(result);
+}
+var result = year==4?"you have 0 year reaming":1;
+if(result !=1){
+    console.log(result);
 }
