3a41e8047782dcad8352a798ddb7e18d10df418f
sandeeep-prajapti <6392424180sandeep@gmail.com>
2023-12-12 09:36:31 +0530

    it is second commit of 12/12/2023

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hello it is 12/12/2023");
+// let num =11;
+// console.log(`modulo with 2 is : ${num%2}`);
+
+// if(num%2 == 0){
+//     console.log(`number is even : ${num}`);
+// }
+// else{
+//     console.log(`number is odd : ${num}`);
+// }
+let open = true;
+if(open){
+console.log(`i will attend the class`);
+}
+else{
+console.log(`collage is closed today`);
+}
